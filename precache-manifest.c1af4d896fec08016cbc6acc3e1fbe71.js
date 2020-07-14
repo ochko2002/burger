b7cc721782bebdcdee73e436bce17d14659c7c2e
ochko2002 <ochko2002@yahoo.com>
2020-07-14 17:59:49 +0800

    Updates

---
+++ precache-manifest.c1af4d896fec08016cbc6acc3e1fbe71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78764fe5260f80e440adb75e4fb58548",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "89d840b66da3d2b73928",
+    "url": "/burger/static/css/main.870e7072.chunk.css"
+  },
+  {
+    "revision": "743e7cda780550d3724b",
+    "url": "/burger/static/js/2.590838f0.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "/burger/static/js/2.590838f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89d840b66da3d2b73928",
+    "url": "/burger/static/js/main.e00c61d7.chunk.js"
+  },
+  {
+    "revision": "626ed8d2a5e67614a6e9",
+    "url": "/burger/static/js/runtime-main.cf849917.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
